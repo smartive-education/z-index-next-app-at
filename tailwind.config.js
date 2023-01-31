a743191fadb8f2c25a85e129f4c329d8cc488457
Attila Toth <tthttl@gmail.com>
2023-01-31 21:01:42 +0100

    fix: add lib to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [
+    require("@smartive-education/design-system-component-z-index/tailwind")
+  ],
   content: [
+    "./node_modules/@smartive-education/design-system-component-z-index/dist/**/*.js",
     "./app/**/*.{ts,tsx}",
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
