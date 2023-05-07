86e7fbd1dbe33f5a863cbf9ba428ba2f3abfa0b0
Attila Toth <tthttl@gmail.com>
2023-05-07 09:30:11 +0200

    fuck this too

--- cypress/e2e/timeline.cy.ts
+++ cypress/e2e/timeline.cy.ts
@@ -1,10 +1,16 @@
 describe('Timeline Page', () => {
   it('should Login', () => {
-    cy.visit('/');
-    cy.get('button').click();
-    cy.get('#loginName').type('newbie@smartive.zitadel.cloud');
-    cy.get('#submit-button').click();
-    cy.get('#password').type('Noob-123');
-    cy.get('#submit-button').click();
+    cy.session('test', () => {
+      cy.visit('/');
+      cy.wait(5000);
+      cy.get('button').click();
+      cy.origin('https://cas-fee-advanced-ocvdad.zitadel.cloud', () => {
+        cy.get('#loginName').type('newbie@smartive.zitadel.cloud');
+        cy.get('#submit-button').click();
+        cy.get('#password').type('Noob-123');
+        cy.get('#submit-button').click();
+      });
+      cy.url({ timeout: 10000 }).should('include', 'localhost:3000');
+    });
   });
 });
