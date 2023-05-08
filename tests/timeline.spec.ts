1e083f1ad66e6c66960ef7b7be1a97b008ccd834
Attila Toth <tthttl@gmail.com>
2023-05-08 21:47:08 +0200

    add e2e tests

--- tests/timeline.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-
-test('Create a Post', async ({ page }) => {
-  await page.goto('https://z-index-next-app-at.vercel.app/');
-  const textarea = await page.waitForSelector(
-    'textarea[data-testid="post-comment-textarea"]'
-  );
-  textarea.fill('Hello World');
-});
