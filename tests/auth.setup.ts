4712a4d3ef3890aef7a3798035e078dc0a82679f
Attila Toth <tthttl@gmail.com>
2023-05-08 19:32:05 +0200

    add login setup

---
+++ tests/auth.setup.ts
@@ -0,0 +1,24 @@
+// auth.setup.ts
+import { test as setup } from '@playwright/test';
+import { authFile } from '../playwright.config';
+
+setup('authenticate', async ({ page }) => {
+  //Login
+  await page.goto('https://z-index-next-app-at.vercel.app/');
+  await page.getByTestId('login-button').click();
+  const input = page.getByPlaceholder('username@domain');
+  await input.fill('newbie@smartive.zitadel.cloud');
+  await page.getByText('next').click();
+  const passwordField = await page.waitForSelector('input[name="password"]');
+  passwordField.fill('Noob-123');
+  await page.getByText('next').click();
+
+  //Make sure we are logged in
+  const textarea = await page.waitForSelector(
+    'textarea[data-testid="post-comment-textarea"]'
+  );
+  await textarea.isVisible();
+
+  //Save the auth state
+  await page.context().storageState({ path: authFile });
+});
--- tests/timeline.spec.ts
+++ tests/timeline.spec.ts
@@ -1,15 +1,9 @@
 import { test } from '@playwright/test';
 
-test('Login', async ({ page }) => {
+test('Create a Post', async ({ page }) => {
   await page.goto('https://z-index-next-app-at.vercel.app/');
-  await page.getByTestId('login-button').click();
-  const input = page.getByPlaceholder('username@domain');
-  await input.fill('newbie@smartive.zitadel.cloud');
-  await page.getByText('next').click();
-  const passwordField = await page.waitForSelector('input[name="password"]');
-  passwordField.fill('Noob-123');
-  await page.getByText('next').click();
-  const textarea = await page.waitForSelector('textarea[data-testid="post-comment-textarea"]');
-  await textarea.isVisible();
+  const textarea = await page.waitForSelector(
+    'textarea[data-testid="post-comment-textarea"]'
+  );
+  textarea.fill('Hello World');
 });
-
