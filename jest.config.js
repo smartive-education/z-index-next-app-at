fb8014141e148cc672becfe27deb8fa747b0b192
Attila Toth <tthttl@gmail.com>
2023-05-13 14:06:22 +0200

    refactor wfs and npm commands

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['<rootDir>/tests'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
